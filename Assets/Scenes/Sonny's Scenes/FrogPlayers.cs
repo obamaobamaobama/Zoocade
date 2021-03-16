eb7bd2737f90d633791edfa8c2a00403b5ffb7fa
Sonny Yang <1014014@student.sae.edu.au>
2021-03-16 17:15:56 +0800

    Sonny aboard!

---
+++ Assets/Scenes/Sonny's Scenes/FrogPlayers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogPlayers : MonoBehaviour
+{
+    
+    public void zCatch()
+    {
+        var player = this.gameObject.name;
+        Debug.Log(player + " Catch");
+    }
+
+    public void zSpit()
+    {
+        var player = this.gameObject.name;
+        Debug.Log(player + " Spit");
+    }
+}
