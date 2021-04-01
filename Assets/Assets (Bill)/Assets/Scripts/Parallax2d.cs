076191cfbdf02805baa350ea7f824a99e18fb359
Bill <1020453@student.sae.edu.au>
2021-04-01 20:07:38 +0800

    Jetpack parallax bg, frog jump folder

---
+++ Assets/Assets (Bill)/Assets/Scripts/Parallax2d.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax2d : MonoBehaviour
+{
+    public float parallaxEffect = 0.5f;
+    private Transform cameraTransform;
+    private Vector3 lastCameraPos;
+    private float textureUnitSizeX;
+       void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPos = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit; //calculate size of the BG sprite
+    }
+
+    void LateUpdate()
+    {
+        Vector3 camMovement = cameraTransform.position - lastCameraPos;
+        
+        transform.position += camMovement * parallaxEffect;
+        lastCameraPos = cameraTransform.position;
+
+        if(Mathf.Abs (cameraTransform.position.x - transform.position.x) >= textureUnitSizeX) //if the camera position minus sprite's position is bigger / equal to texture size
+        {
+            float offsetPosX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX; //getting the remainder of the position based of texture size
+            transform.position = new Vector3(cameraTransform.position.x + offsetPosX, transform.position.y);
+        }
+    }
+}
