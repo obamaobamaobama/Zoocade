ed60b26f31163ddbef8b3d4692fccfac45ce60b6
Bill <1020453@student.sae.edu.au>
2021-04-04 11:44:30 +0800

    Added debug console to my scenes

---
+++ Assets/Assets (Bill)/Assets/Scripts/Dev Console/DebugCommandBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; // for the action delegate type down in DebugCommand class
+
+public class DebugCommandBase //new class for making commands
+{
+   private string _commandId;
+   private string _commandDescription;
+   private string _commandFormat;
+
+   public string commandId {get {return _commandId;} }
+   public string commandDescription {get{return _commandDescription;}}
+
+   public string commandFormat {get{return _commandFormat;}}
+
+   public DebugCommandBase(string id, string description, string format)
+   {
+      _commandId = id;
+      _commandDescription = description;
+      _commandFormat = format;
+   }
+}
+public class DebugCommand : DebugCommandBase
+{
+   private Action command; // action does not return a value, but delegate does
+   public DebugCommand(string id, string description, string format, Action command): base (id, description, format)
+   {
+      this.command = command;
+   }
+
+   public void Invoke()
+   {
+      command.Invoke();
+   }
+  
+}
+ public class DebugCommand<T1> : DebugCommandBase // command with an int you want to put it
+ {
+    private Action<T1> command;
+   public DebugCommand (string id, string description, string format, Action<T1> command): base (id, description, format)
+   {
+      this.command = command;
+   }
+   public void Invoke(T1 value)
+   {
+      command.Invoke(value);
+   }
+
+ }
---
+++ Assets/Assets (Bill)/Assets/Scripts/Dev Console/DebugController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DebugController : MonoBehaviour
+{
+   bool showConsole;
+   string input;
+   //list of commands here
+   public List<object> commandList;
+    public static DebugCommand SKIP;
+   public static DebugCommand<int> PLAY_GAME;
+   
+   
+  
+
+       void Awake()
+    {
+        PLAY_GAME = new DebugCommand<int>("!play", "load game", "play",(x) => //(command line, description, reads format)
+        {
+            SceneManager.LoadScene(x);
+        });
+
+        commandList = new List<object>
+        {
+            PLAY_GAME,
+            SKIP
+        };
+
+    }
+    public void Update()
+   {
+       if(Input.GetKeyDown(KeyCode.BackQuote)) // open console
+       {
+           Debug.Log("Show Console");
+           OnToggleDebug();
+       }
+       if (Input.GetKeyDown(KeyCode.Return))
+       {
+           Debug.Log("enter");
+           OnReturn();
+       }
+   }
+    public void OnToggleDebug()
+     {
+         showConsole = !showConsole;
+     }
+     public void OnReturn()
+     {
+         if(showConsole)
+         {
+             HandleInput();
+             input = "";
+         }
+     }
+
+     //private void /// <summary>
+     /// OnGUI is called for rendering and handling GUI events.
+     /// This function can be called multiple times per frame (one call per event).
+     /// <///summary>
+     void OnGUI()
+     {
+         if (!showConsole) {return;}
+
+         float y = 0f;
+
+         GUI.Box(new Rect(0, y, Screen.width, 30),"");
+         GUI.backgroundColor = new Color(0, 0, 0, 0);
+         input = GUI.TextField(new Rect(10f, y + 5f, Screen.width-20f, 20f), input);
+
+        if (Event.current.keyCode == KeyCode.Return)
+       {
+           Debug.Log("enter");
+           OnReturn();
+       }
+     }
+    private void HandleInput()
+    {
+        string[] properties = input.Split(' '); //handles another variable when there is a space
+        for(int i=0; i<commandList.Count; i++)
+        {
+            DebugCommandBase commandBase = commandList[i] as DebugCommandBase;
+
+            if (input.Contains(commandBase.commandId))
+            {
+                if(commandList[i] as DebugCommand != null)
+                {
+                    //cast to this type and incoke the command
+                    (commandList[i] as DebugCommand).Invoke();
+                }
+                else if (commandList[i] as DebugCommand<int> != null)
+                {
+                    (commandList[i] as DebugCommand<int>).Invoke(int.Parse(properties[1]));
+              
+                }
+            }
+        }
+    }
+}
