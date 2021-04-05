c92faac8601979a3bd2691f46ab3315a04b4668b
Sonny Yang <1014014@student.sae.edu.au>
2021-04-05 21:55:54 +0800

    Added components and script to P1 fly

---
+++ Assets/Assets (Sonny)/Frog Meditation/Fly.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    private float flySpeed;
+	public bool flyGo;
+
+	private void Start()
+	{
+		flySpeed = GameObject.Find("FrogMeditationManager").GetComponent<FrogMeditationManager>().flySpeed;
+	}
+
+	private void Update()
+	{
+		if (flyGo)
+		{
+			if (this.GetComponent<RectTransform>().position.x < -7)
+			{
+				var RM = GameObject.Find("FrogMeditationManager");
+				RM.GetComponent<FrogMeditationManager>().zTimesUp();
+			}
+			else
+			{
+				this.GetComponent<RectTransform>().localPosition += new Vector3(0, -flySpeed) * Time.deltaTime;
+			}
+		}
+	}
+}
